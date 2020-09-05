ee8aaf401fc970d15607583c7a5c4b50c3587057
TBSDrJ <marcus.jaiclin@bishops.com>
2020-09-05 16:39:25 -0700

    Add predictions, check accuracy.

--- kMeans.py
+++ kMeans.py
@@ -15,3 +15,22 @@ estimator = sklearn.cluster.KMeans(
     n_init = 10,
 )
 estimator.fit(sklearn.preprocessing.scale(digitsX))
+predY = estimator.predict(digitsX)
+
+finalPreds = [0 for i in range(10)]
+for label in range(10):
+    counts = [0 for i in range(10)]
+    for pred, gt in zip(predY, digitsY):
+        if pred == label:
+            counts[gt] += 1
+    finalPreds[label] = counts.index(max(counts))
+
+correct = 0
+incorrect = 0
+for i in range(len(predY)):
+    predY[i] = finalPreds[predY[i]]
+for pred, gt in zip(predY, digitsY):
+    if pred == gt: correct += 1
+    if pred != gt: incorrect += 1
+print(correct, incorrect)
+print(f"{correct/len(predY):5.2} correct")
