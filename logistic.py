ebbf13bf5b0a1579b7df1c3d7086379c20c73cb3
TBSDrJ <marcus.jaiclin@bishops.com>
2020-09-06 15:00:42 -0700

    Adjust params, get results.

--- logistic.py
+++ logistic.py
@@ -3,6 +3,9 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
+# matplotlib 3.3.1
+from matplotlib import pyplot
 
 digits = load_digits()
 digitsX = digits.images.reshape(len(digits.images), 64)
@@ -11,6 +14,16 @@ trainX, testX, trainY, testY = train_test_split(
     digitsX, digitsY, test_size = 0.3, shuffle = True
     )
 
-classifier = LogisticRegression()
+classifier = LogisticRegression(max_iter = 10000)
 classifier.fit(trainX, trainY)
 preds = classifier.predict(testX)
+
+correct = 0
+incorrect = 0
+for pred, gt in zip(preds, testY):
+    if pred == gt: correct += 1
+    else: incorrect += 1
+print(f"Correct: {correct}, Incorrect: {incorrect}, % Correct: {correct/(correct + incorrect): 5.2}")
+
+plot_confusion_matrix(classifier, testX, testY)
+pyplot.show()
