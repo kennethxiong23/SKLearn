3882b13e562c06ae151229dec47b4fdaec66ba99
TBSDrJ <marcus.jaiclin@bishops.com>
2020-09-05 17:05:11 -0700

    Split data, build model.

--- linearReg.py
+++ linearReg.py
@@ -3,8 +3,18 @@
 import sklearn.linear_model
 import sklearn.datasets
 import sklearn.preprocessing
+import sklearn.model_selection
 
 digits = sklearn.datasets.load_digits()
 digitsX = digits.images
 digitsX = digitsX.reshape((len(digitsX), 64))
 digitsY = digits.target
+
+trainX, testX, trainY, testY = sklearn.model_selection.train_test_split(
+    digitsX, digitsY, test_size = 0.2, shuffle = True
+)
+
+regModel = sklearn.linear_model.LinearRegression()
+regModel.fit(trainX, trainY)
+
+print(regModel.coef_)
